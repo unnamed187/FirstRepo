c32b1944de7eea8c34ba02c0dd3dc3d6a4f7aee9
unnamed187 <unnamed.number.187@gmail.com>
2016-10-13 17:14:51 +0500

    Update data_types.R

--- lab1/data_types.R
+++ lab1/data_types.R
@@ -1,17 +1,19 @@
 # Присвойте значение 33.3 переменной my_numeric
-my_numeric <- 
-# Проверьте тип данных переменной
-class(my_numeric)
-
+my_numeric <- 33.3
+#my_numeric
+  # Проверьте тип данных переменной
+  class(my_numeric)
+  typeof(my_numeric)
 # Смените значение my_numeric на 42
-
+my_numeric<-42
 
 # Проверьте тип данных my_numeric
-
+typeof(my_numeric)
 # Установите значение my_character в "МИР"
-my_character <- "Привет"
-# Проверьте тип данных my_numeric
-
+my_character <- "МИР"
+# Проверьте тип данных my_character
+typeof(my_character)
 # Установите my_logical в FALSE
-my_logical <- TRUE
+my_logical <- FALSE
 # Проверьте тип данных my_numeric
+typeof(my_logical)
