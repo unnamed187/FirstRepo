48eb7a7f07050e20590140ef719d5e1384180a60
unnamed187 <unnamed.number.187@gmail.com>
2016-10-13 17:15:38 +0500

    Create data_types_conversion.R

---
+++ lab1/data_types_conversion.R
@@ -0,0 +1,15 @@
+# Проверка и преобразование типов is.XYS as.XYZ 
+
+# Присвойте значение 33.3 переменной my_numeric
+my_numeric <- 33.3
+  
+  # Установите значение my_character в "33"
+  my_character <- "33"
+# Проверьте тип данных my_character
+is.numeric(my_character)
+# Преобразуйте my_character в число при помощи функции as.numeric и запишите в переменную my_numeric_character
+
+
+my_numeric_character<- as.numeric(my_character)
+  # Пребразуйте сумму my_numeric и my_numeric_character и выведите на экран
+my_numeric_character+as.numeric(my_character)
